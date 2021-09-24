e43aaf1be8b6af798b741a99e1043cda9e87d0e2
samwatin <62219325+samwatin@users.noreply.github.com>
2021-09-24 20:23:57 +0600

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -8,6 +8,7 @@ public class AddressBook {
     public static void main (String[] args){
         System.out.println("Address Book");
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", 613);
+        BuddyInfo buddy2 = new BuddyInfo("Sam", "Carleton", 614);
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
